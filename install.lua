e31d911c12d29da9a32c127747584f0e7b69cd11
Src-Beka <89603835+Src-Beka@users.noreply.github.com>
2021-08-27 06:30:42 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -27,6 +27,13 @@ if not database:get(Server_Done.."Token_Write") then
 print('\27[0;31m\n ارسل لي توكن البوت الان ↓ :\na┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n\27')
 local token = io.read()
 if token ~= '' then
+data,res = https.request("https://Beka-api.ml/index.php?p=Beka-DeV")
+if res == 200 then
+tr = json:decode(data)
+if tr.Info.info == 'Is_Spam' then
+io.write('\n\27[1;31m'..tr.Info.info..'\n\27[0;39;49m')
+os.execute('lua install.lua')
+end 
 if tr.Info.info == 'Ok' then
 local url , res = https.request('https://api.telegram.org/bot'..token..'/getMe')
 if res ~= 200 then
@@ -46,13 +53,23 @@ if not database:get(Server_Done.."UserSudo_Write") then
 print('\27[0;35m\n ارسل لي ايدي المطور الاساسي ↓ :\na┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n\27[0;33;49m')
 local Id = io.read():gsub(' ','') 
 if tostring(Id):match('%d+') then
+data,res = https.request("https://Beka-api.ml/index.php?bn=info&id="..Id)
+if res == 200 then
+muaed = json:decode(data)
+if muaed.Info.info == 'Is_Spam' then
+io.write('\n\27[1;35m عذرا الايدي محظور من السورس \n\27[0;39;49m') 
+os.execute('lua start.lua')
+end 
+if muaed.Info.info == 'Ok' then
 io.write('\27[1;35m تم حفظ ايدي المطور الاساسي \na┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n27[0;39;49m')
-database:set(Server_Done.."UserSudo_Write",Id) 
+database:set(Server_Done.."UserSudo_Write",Id)
+end 
 else
 io.write('\27[0;31m┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n لم يتم حفظ ايدي المطور الاساسي ارسله مره اخره')
 end
 os.execute('lua install.lua')
 end 
+end
 if not database:get(Server_Done.."User_Write") then
 print('\27[1;31m ↓ ارسل معرف المطور الاساسي :\n SEND ID FOR SIDO : \27[0;39;49m')
 local User = io.read():gsub('@','') 
@@ -66,7 +83,7 @@ os.execute('lua install.lua')
 end
 local function Files_Info_Get()
 Create_Info(database:get(Server_Done.."Token_Write"),database:get(Server_Done.."UserSudo_Write"),database:get(Server_Done.."User_Write")) 
-local RunBot = io.open("TELAND", 'w')
+local RunBot = io.open("Beka", 'w')
 RunBot:write([[
 #!/usr/bin/env bash
 cd $HOME/Beka
@@ -95,7 +112,7 @@ Files_Info_Get()
 database:del(Server_Done.."User_Write");database:del(Server_Done.."Token_Write");database:del(Server_Done.."UserSudo_Write")
 sudos = dofile('Info.lua')
 os.execute('./install.sh ok')
-
+end 
 local function Load_File()  
 local f = io.open("./Info.lua", "r")  
 if not f then   
