4a6d98144d79d3fe49ca147d75b9c17283d2b4e0
Src-Beka <89603835+Src-Beka@users.noreply.github.com>
2021-11-01 08:23:45 +0300

    Update Beka.lua

--- Beka.lua
+++ Beka.lua
@@ -2618,7 +2618,7 @@ end
 end,nil)   
 end
 if text == 'السورس' or text == 'سورس' or text == 'ياسورس' or text == 'يا سورس' then  
-Text = "ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sᴏᴜʀᴄᴇ ʙᴇᴋᴀ\n\n[⌁  ʙᴇᴋᴀ ᴄʜᴀɴɴᴇʟ](http://t.me/BVVBV5)\n\n[⌁  ɪɴғᴏ sᴏᴜʀᴄᴇ](http://t.me/BVVBV5)\n\n[⌁  ʙᴇᴋᴀ ᴅᴇᴠᴇʟᴏᴘᴇʀ](http://t.me/JJJJOJJ)\n\n[⌁  ʙᴇᴋᴀ ᴅᴇᴠᴇʟᴏᴘᴇʀ](http://t.me/YIII00)"
+Text = "ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sᴏᴜʀᴄᴇ ʙᴇᴋᴀ\n\n[⌁  ʙᴇᴋᴀ ᴄʜᴀɴɴᴇʟ](http://t.me/BVVBV5)\n\n[⌁  ɪɴғᴏ sᴏᴜʀᴄᴇ](http://t.me/BVVBV5)\n\n[⌁  ʙᴇᴋᴀ ᴅᴇᴠᴇʟᴏᴘᴇʀ](http://t.me/EENE0)\n\n[⌁  ʙᴇᴋᴀ ᴅᴇᴠᴇʟᴏᴘᴇʀ](http://t.me/YIII00)"
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '⌁ sᴏᴜʀᴄʀ ʙᴇᴋᴀ',url="t.me/BVVBV5"}},
