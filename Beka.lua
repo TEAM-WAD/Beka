e21286efdad91a25a9142355344597afc6dacec5
Src-Beka <89603835+Src-Beka@users.noreply.github.com>
2021-11-05 19:11:43 -0700

    Update Beka.lua

--- Beka.lua
+++ Beka.lua
@@ -2624,7 +2624,7 @@ keyboard.inline_keyboard = {
 {{text = '𓅛︙S𝗼U𝗿C𝗲 B𝗲K𝗮',url="t.me/Z_ZW1"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/Z_ZW1&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/BE_KA0&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 --------------------------------------------------------------------------------------------------------------
 if Chat_Type == 'GroupBot' and ChekAdd(msg.chat_id_) == true then
