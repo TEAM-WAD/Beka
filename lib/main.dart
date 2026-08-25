23fc48caccf683e903f95211de138fda8db895ff
TEAM-WAD <92700528+TEAM-WAD@users.noreply.github.com>
2026-08-25 05:47:26 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TelegramApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TelegramApp extends StatelessWidget {
+  const TelegramApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Telegram',
+      theme: ThemeData.dark(),
+      home: const TelegramScreen(),
+    );
+  }
+}
+
+class TelegramScreen extends StatefulWidget {
+  const TelegramScreen({super.key});
+
+  @override
+  State<TelegramScreen> createState() => _TelegramScreenState();
+}
+
+class _TelegramScreenState extends State<TelegramScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent(
+        "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+      )
+      ..loadRequest(
+        Uri.parse('https://web.telegram.org/k/'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
